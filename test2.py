8b0cf0b1313182b9f35acabf6ad66304cd49839a
heberttt <hebertp054@gmail.com>
2024-03-04 14:19:11 +0800

    add a note

--- test2.py
+++ test2.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+#add headless browser
 
 driver = webdriver.Edge()
 
