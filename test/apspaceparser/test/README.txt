a194e9599d5fd5918bea468ec3aaffb7296e71e8
hebertpratama <TP067551@mail.apu.edu.my>
2024-03-11 22:10:31 +0800

    v2.0.0 added hr_desperate and grouping filter

--- test/apspaceparser/test/README.txt
+++ test/apspaceparser/test/README.txt
@@ -3,3 +3,9 @@
 - If a ta have class right before the shift, the shift will not be included in fear of class delay(ex: i have class at 1:30-2:30, the shift at 2:30-3:30 will not be included)
 - If there are more than 1 group in their intake, it will calculate all the timetables of all groups.
 
+
+hr:
+Type "hr" when prompt "Custom (Enter if not): " appears
+- This will show you all the available TAs on that specific shift
+
+Typing "hr_desperate" when prompt "Custom (Enter if not) : " appears will remove the "If a ta have class right before the shift, the shift will not be included in fear of class delay(ex: i have class at 1:30-2:30, the shift at 2:30-3:30 will not be included)" condition
--- test/apspaceparser/test/apspaceparser_test.dart
+++ test/apspaceparser/test/apspaceparser_test.dart
@@ -140,6 +140,110 @@ void main() async {
             
           break;
         }}
+      }else if (special == "hr_desperate") {
+        var technicalAssistantsIntakes = await getJson(getPath());
+
+        List<dynamic> taData = technicalAssistantsIntakes;
+        while (true) {
+          Date thisMon1 = getThisWeekDate();
+          int weekQuantity = getNumberOfWeeks(jsonData);
+          String nextWeek = "";
+          String thirdWeek = "";
+
+          String thisWeek = "${thisMon1.date}-${thisMon1.month}-${thisMon1.year}";
+          if (weekQuantity > 1){
+            thisMon1.addDate(7);
+            nextWeek = "${thisMon1.date}-${thisMon1.month}-${thisMon1.year}";
+          }
+          if (weekQuantity > 2){
+            thisMon1.addDate(7);
+            thirdWeek = "${thisMon1.date}-${thisMon1.month}-${thisMon1.year}";
+          }
+          
+          
+          if (weekQuantity == 1){
+            print("Available weeks: $thisWeek");
+          }else if (weekQuantity == 2){
+            print("Available weeks: $thisWeek, $nextWeek");
+          }else if (weekQuantity > 2){
+            print("Available weeks : $thisWeek, $nextWeek, $thirdWeek");
+          }
+
+          
+
+          print("Enter the week (ex:4-3-2024): ");
+          String? selectedWeek = stdin.readLineSync();
+          print("Enter the day (ex:Monday): ");
+          String? selectedDay = stdin.readLineSync();
+          print("Enter the shift (Just enter from 1 to 10) : ");
+          String? selectedShift = stdin.readLineSync();
+
+          if (selectedWeek == "" || selectedDay == "" || selectedShift == "") {
+            print("Please enter again");
+            continue;
+          }
+          else{
+            List<dynamic> allFreeShifts = [];
+            for(int i = 0; i < weekQuantity; i++){
+              List<dynamic> freeShiftsWeek = getDesperateAllWeekFreeShifts(taData, jsonData, i + 1);
+              allFreeShifts.add(freeShiftsWeek);
+            }
+
+            List<dynamic> freeShiftForThisWeek = [];
+            List<String> availableTas = [];
+            List<String> ambiguousTas = [];
+
+            if (selectedWeek == thisWeek){
+              freeShiftForThisWeek = allFreeShifts[0];
+              for(int i = 0; i < freeShiftForThisWeek.length; i++){
+                String freeShiftStr = freeShiftForThisWeek[i]['Schedule'][selectedDay];
+                List<String> freeShiftArr = freeShiftStr.split(",");
+                if (freeShiftArr.contains("S$selectedShift")){
+                  availableTas.add(freeShiftForThisWeek[i]['name'] + " (${freeShiftForThisWeek[i]['intake']})");
+                }
+                if (freeShiftStr == ''){
+                  ambiguousTas.add(freeShiftForThisWeek[i]['name'] + " (${freeShiftForThisWeek[i]['intake']})");
+                }
+              }
+              
+            }else if (selectedWeek == nextWeek){
+              freeShiftForThisWeek = allFreeShifts[1];
+              for(int i = 0; i < freeShiftForThisWeek.length; i++){
+                String freeShiftStr = freeShiftForThisWeek[i]['Schedule'][selectedDay];
+                List<String> freeShiftArr = freeShiftStr.split(",");
+                if (freeShiftArr.contains("S$selectedShift")){
+                  availableTas.add(freeShiftForThisWeek[i]['name'] + " (${freeShiftForThisWeek[i]['intake']})");
+                }if (freeShiftStr == ''){
+                  ambiguousTas.add(freeShiftForThisWeek[i]['name'] + " (${freeShiftForThisWeek[i]['intake']})");
+                }
+              }
+            }else if (selectedWeek == thirdWeek){
+              freeShiftForThisWeek = allFreeShifts[2];
+              for(int i = 0; i < freeShiftForThisWeek.length; i++){
+                String freeShiftStr = freeShiftForThisWeek[i]['Schedule'][selectedDay];
+                List<String> freeShiftArr = freeShiftStr.split(",");
+                if (freeShiftArr.contains("S$selectedShift")){
+                  availableTas.add(freeShiftForThisWeek[i]['name'] + " (${freeShiftForThisWeek[i]['intake']})");
+                }if (freeShiftStr == ''){
+                  ambiguousTas.add(freeShiftForThisWeek[i]['name'] + " (${freeShiftForThisWeek[i]['intake']})");
+                }
+              }
+            }
+            print("");
+            getUnknownIntake(taData);
+            print("");
+            if(ambiguousTas.isNotEmpty){
+              print("$ambiguousTas, these TA's schedule is not available yet for this week (Either on holiday or week not generated yet by ApSpace.)");
+              print("");
+            }
+            
+            print("These TA are available at S$selectedShift on $selectedDay for the Week of $selectedWeek: ");
+            for (var technicalAssistant in availableTas){
+              print(technicalAssistant);
+            }
+            
+          break;
+        }}
       } else {
         print("Enter intake: ");
 
